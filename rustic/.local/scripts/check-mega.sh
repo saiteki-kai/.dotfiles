a881c56eab692b32303aa9c367829395af1a5046
saiteki-kai <giuseppe.magazu@gmail.com>
2024-09-18 19:40:41 +0200

    feat: add exit 1

--- rustic/.local/scripts/check-mega.sh
+++ rustic/.local/scripts/check-mega.sh
@@ -11,4 +11,5 @@ if [ "$MEGA_USED" -gt "$MEGA_TOTAL" ]; then
     MEGA_USED_READABLE=$(numfmt --to=iec-i --suffix=B --format="%.2f" "$MEGA_USED")
 
     notify-send "Backup Error" "The MEGA storage is full at $MEGA_USED_READABLE" --icon=gtk-no --app-name="Rustic"
+    exit 1
 fi
