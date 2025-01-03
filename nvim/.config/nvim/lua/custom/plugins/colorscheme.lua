2178c373215ef62e003c933f3c8e904864f5d3dc
saiteki-kai <giuseppe.magazu@gmail.com>
2025-01-03 23:30:14 +0100

    feat: update neovim ui config

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -10,30 +10,11 @@ return {
         integrations = {
             mason = true,
             snacks = true,
+            fidget = true,
             blink_cmp = true,
-            -- native_lsp = {
-            --     enabled = true,
-            --     virtual_text = {
-            --         errors = { "italic" },
-            --         hints = { "italic" },
-            --         warnings = { "italic" },
-            --         information = { "italic" },
-            --         ok = { "italic" }
-            --     },
-            --     underlines = {
-            --         errors = { "underline" },
-            --         hints = { "underline" },
-            --         warnings = { "underline" },
-            --         information = { "underline" },
-            --         ok = { "underline" }
-            --     },
-            --     inlay_hints = {
-            --         background = true
-            --     }
-            -- },
-        }
+       }
     },
     init = function()
-        vim.cmd([[colorscheme catppuccin]])
+      vim.cmd([[colorscheme catppuccin]])
     end
 }
--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -4,32 +4,33 @@ return {
     { "nvim-lua/plenary.nvim",       lazy = true },
     { "nvim-tree/nvim-web-devicons", lazy = true },
 
-    -- Special Comments
+    -- Special comments
     {
         "folke/todo-comments.nvim",
         event = { 'BufReadPost', 'BufNewFile' },
         opts = { signs = false },
     },
 
-    -- Block Comments
+    -- Block comments
     {
         "numToStr/Comment.nvim",
         event = { 'BufReadPost', 'BufNewFile' },
         opts = {},
     },
-    { "stevearc/dressing.nvim", event = "VeryLazy" },
+
+    -- Input ui
+    {
+        "stevearc/dressing.nvim",
+        event = "VeryLazy"
+    },
 }
 
 -- TODO:
---  0       nvim :tutor
 --  1       configure telescope
---  1.5     add keymap for todo quicklist to telescope
---  2       filepath in the status line
---  3       indentation keymap
---  4       git setup (commit tree, status) using lazygit integrated with snacks.nvim
---  5       rewrite LSP configuration
---  6       refine completion with AI
---  7       formatting ?
---  8       multi cursor
---  9       debugger / diagnostics / quickfix lists
---  10      neogen (documentation generation)
+--  2       add keymap for quickfix
+--  3       rewrite LSP configuration
+--  4       formatting ?
+--  5       multi cursor
+--  6       debugger / diagnostics / quickfix lists
+--  7       refine completion with AI
+--  8      neogen (documentation generation)
