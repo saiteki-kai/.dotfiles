381a8bf1379f408e84c49be92e3037b1bb50d28f
saiteki-kai <giuseppe.magazu@gmail.com>
2025-01-07 18:20:42 +0100

    feat: update lsp configuration

--- nvim/.config/nvim/lua/custom/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/custom/plugins/lsp-config.lua
@@ -1,146 +1,162 @@
 ---@module "lazy"
 ---@type LazySpec
 return {
-    "neovim/nvim-lspconfig",
+    {
+        "williamboman/mason.nvim",
 
-    event = { 'BufReadPre', 'BufNewFile' },
+        lazy = false,
 
-    dependencies = {
-        'saghen/blink.cmp',
-        {
-            "folke/lazydev.nvim",
-            ft = "lua",
-            opts = {
-                library = {
-                    "snacks.nvim",
-                    { path = "lazy.nvim", words = { "Lazy" } },
-                    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-                },
+        ---@module "mason"
+        ---@type MasonSettings
+        opts = {
+            ui = {
+                border = "rounded",
             },
         },
-        {
-            "williamboman/mason.nvim",
-            -- opts = {
-            --     icons = {
-            --         package_installed = "✓",
-            --         package_pending = "➜",
-            --         package_uninstalled = "✗",
-            --     },
-            -- }
-        },
-        "williamboman/mason-lspconfig.nvim",
     },
+    {
+        "neovim/nvim-lspconfig",
 
-    config = function()
-        local lspconfig = require("lspconfig")
-        local capabilities = require('blink.cmp').get_lsp_capabilities()
+        event = { "BufReadPre", "BufNewFile" },
 
-        require("mason").setup({})
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "lua_ls",
-                "rust_analyzer",
-            },
-            automatic_installation = false,
-            handlers = {
-                function(server_name)
-                    lspconfig[server_name].setup({
-                        capabilities = capabilities
-                    })
-                end,
-                ["lua_ls"] = function()
-                    lspconfig.lua_ls.setup {
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                diagnostics = {
-                                    globals = { "vim" }
-                                }
-                            }
-                        }
-                    }
-                end,
-            }
-        })
+        dependencies = {
+            "saghen/blink.cmp",
+            "williamboman/mason-lspconfig.nvim",
+        },
 
-        -- lspconfig.rust_analyzer.setup({
-        --     capabilities = capabilities,
-        --     cmd = {
-        --         "rustup",
-        --         "run",
-        --         "stable",
-        --         "rust-analyzer",
-        --     },
-        --     settings = {
-        --         ["rust-analyzer"] = {
-        --             cargo = { allFeatures = true },
-        --             checkOnSave = true,
-        --             check = {
-        --                 enable = true,
-        --                 command = "clippy",
-        --                 features = "all",
-        --             },
-        --         },
-        --     },
-        -- })
+        config = function()
+            local lspconfig = require("lspconfig")
 
-        -- lspconfig.pyright.setup({
-        --     capabilities = capabilities,
-        --     settings = {
-        --         pyright = {
-        --             -- Using Ruff's import organizer
-        --             disableOrganizeImports = true,
-        --         },
-        --         python = {
-        --             analysis = {
-        --                 -- Ignore all files for analysis to exclusively use Ruff for linting
-        --                 ignore = { '*' },
-        --             },
-        --         },
-        --     },
-        -- })
-        --
-        -- lspconfig.ruff.setup({
-        --     capabilities = capabilities,
-        --     trace = 'messages',
-        --     init_options = {
-        --         settings = {
-        --             logLevel = 'debug',
-        --         }
-        --     }
-        -- })
+            local capabilities = vim.tbl_deep_extend(
+                "force",
+                {},
+                vim.lsp.protocol.make_client_capabilities(),
+                require("blink.cmp").get_lsp_capabilities()
+            )
 
-        --     vim.api.nvim_create_autocmd("LspAttach", {
-        --         callback = function(args)
-        --             local client = vim.lsp.get_client_by_id(args.data.client_id)
-        --             if not client then
-        --                 return
-        --             end
-        --
-        --             vim.api.nvim_create_autocmd("BufWritePre", {
-        --                 buffer = args.buf,
-        --                 callback = function()
-        --                     vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
-        --                 end,
-        --             })
-        --         end,
-        --         desc = "Format on save"
-        --     })
-        --
-        --     vim.api.nvim_create_autocmd("LspAttach", {
-        --         group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
-        --         callback = function(args)
-        --             local client = vim.lsp.get_client_by_id(args.data.client_id)
-        --             if client == nil then
-        --                 return
-        --             end
-        --             if client.name == 'ruff' then
-        --                 -- Disable hover in favor of Pyright
-        --                 client.server_capabilities.hoverProvider = false
-        --             end
-        --         end,
-        --         desc = 'LSP: Disable hover capability from Ruff',
-        --     })
-        -- end
-    end
+            require("mason").setup({})
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "rust_analyzer",
+                },
+                automatic_installation = false,
+                handlers = {
+                    function(server_name)
+                        lspconfig[server_name].setup({
+                            capabilities = capabilities,
+                        })
+                    end,
+                    ["lua_ls"] = function()
+                        lspconfig.lua_ls.setup({
+                            capabilities = capabilities,
+                            settings = {
+                                Lua = {
+                                    runtime = { version = "LuaJIT" },
+                                    workspace = {
+                                        checkThirdParty = false,
+                                    },
+                                    diagnostics = {
+                                        globals = { "vim" },
+                                    },
+                                    completion = {
+                                        callSnippet = "Replace",
+                                    },
+                                },
+                            },
+                        })
+                    end,
+                },
+            })
+
+            vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+                border = "rounded",
+            })
+            vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+                border = "rounded",
+            })
+
+            require("lspconfig.ui.windows").default_options.border = "rounded"
+
+            -- lspconfig.rust_analyzer.setup({
+            --     capabilities = capabilities,
+            --     cmd = {
+            --         "rustup",
+            --         "run",
+            --         "stable",
+            --         "rust-analyzer",
+            --     },
+            --     settings = {
+            --         ["rust-analyzer"] = {
+            --             cargo = { allFeatures = true },
+            --             checkOnSave = true,
+            --             check = {
+            --                 enable = true,
+            --                 command = "clippy",
+            --                 features = "all",
+            --             },
+            --         },
+            --     },
+            -- })
+
+            -- lspconfig.pyright.setup({
+            --     capabilities = capabilities,
+            --     settings = {
+            --         pyright = {
+            --             -- Using Ruff's import organizer
+            --             disableOrganizeImports = true,
+            --         },
+            --         python = {
+            --             analysis = {
+            --                 -- Ignore all files for analysis to exclusively use Ruff for linting
+            --                 ignore = { '*' },
+            --             },
+            --         },
+            --     },
+            -- })
+            --
+            -- lspconfig.ruff.setup({
+            --     capabilities = capabilities,
+            --     trace = 'messages',
+            --     init_options = {
+            --         settings = {
+            --             logLevel = 'debug',
+            --         }
+            --     }
+            -- })
+
+            --     vim.api.nvim_create_autocmd("LspAttach", {
+            --         callback = function(args)
+            --             local client = vim.lsp.get_client_by_id(args.data.client_id)
+            --             if not client then
+            --                 return
+            --             end
+            --
+            --             vim.api.nvim_create_autocmd("BufWritePre", {
+            --                 buffer = args.buf,
+            --                 callback = function()
+            --                     vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+            --                 end,
+            --             })
+            --         end,
+            --         desc = "Format on save"
+            --     })
+            --
+            --     vim.api.nvim_create_autocmd("LspAttach", {
+            --         group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
+            --         callback = function(args)
+            --             local client = vim.lsp.get_client_by_id(args.data.client_id)
+            --             if client == nil then
+            --                 return
+            --             end
+            --             if client.name == 'ruff' then
+            --                 -- Disable hover in favor of Pyright
+            --                 client.server_capabilities.hoverProvider = false
+            --             end
+            --         end,
+            --         desc = 'LSP: Disable hover capability from Ruff',
+            --     })
+            -- end
+        end,
+    },
 }
---
+++ nvim/TODO.md
@@ -0,0 +1,24 @@
+# TODO
+
+Next steps:
+
+- [x] background, theme, transparency
+- [ ] rewrite LSP configuration
+- [ ] nvim tree
+- [ ] configure telescope
+- [ ] multi cursor, change indenting keybindings
+- [ ] debugger / diagnostics (trouble.nvim, ..) / quickfix lists
+- [ ] ai completion
+- [ ] neogen (documentation generation)
+- [ ] view git edits in a line / block
+
+Other plugins to investigate:
+
+- aerial (code outline with sidebar for definitions, ...)
+- code-companion (ai aggregator)
+- sindrets/diffview.nvim (git history and diff view)
+- dnlhc/glance.nvim (view definition and reference previews)
+- kevinhwang91/nvim-ufo (code folds)
+- markdown rendering
+- latex rendering
+- obsidian.nvim
