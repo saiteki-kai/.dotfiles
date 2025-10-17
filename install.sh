76e5938106a292716636bb5f65fe840e74a4db3d
saiteki-kai <giuseppe.magazu@gmail.com>
2025-10-17 12:14:39 +0200

    feat: enhance install script for specific window manager support

--- install.sh
+++ install.sh
@@ -1,8 +1,33 @@
-#!/bin/sh
-# Install dotfiles using stow
+#!/bin/bash
+# Install dotfiles using stow for a specific window manager.
+# Window managers are mutually exclusive as same config files may be used (e.g. waybar, rofi, etc.)
+#
+# Usage: ./install.sh <wm>
+
+AVAILABLE_WMS=("hyprland" "sway")
+
+WM=$1
+
+# check if wm is available
+# shellcheck disable=SC2076
+if  [[ ! " ${AVAILABLE_WMS[*]} " =~ " ${WM} " ]]; then
+    echo "Usage: $0 <wm>"
+    echo "Available window managers: ${AVAILABLE_WMS[*]}"
+    exit 1
+fi
 
 for folder in */; do
-    echo "stow $folder"
+    # skip every WM except the one passed as argument
+    for wm in "${AVAILABLE_WMS[@]}"; do
+        if [[ "$folder" == "$wm/" && "$wm" != "$WM" ]]; then
+            echo "[unstow ${folder//\//}]"
+            stow -t "$HOME" -D "$folder"
+            printf "\n"
+            continue 2
+        fi
+    done
+
+    echo "[stow ${folder//\//}]"
 
     stow -t "$HOME" -D "$folder"
     stow -t "$HOME" -v "$folder"
